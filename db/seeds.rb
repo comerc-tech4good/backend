fb8f9bc4e67a4820f2207dfa7da56bf41f77631c
Nestor Martinez <nesturron@gmail.com>
2023-06-20 18:24:48 +0200

    Imported database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Activity.destroy_all
+District.destroy_all
+
+#####################################
+#                                   #
+#                                   #
+#             Activites             #
+#                                   #
+#                                   #
+#####################################
 
 column_names = File.readlines('db/data/message.txt').map(&:strip)
 
@@ -16,4 +25,25 @@ column_names[7..column_names.size - 1].each do |column_name|
   Activity.create!(
     name: column_name.capitalize.gsub('_', ' ')
   )
+end
+
+#####################################
+#                                   #
+#                                   #
+#             DATABASE              #
+#                                   #
+#                                   #
+#####################################
+
+require 'smarter_csv'
+
+database_file_path = 'db/data/tabla.csv'
+
+options = {
+  # chunk_size: 1000, # Adjust this number based on your CSV file size
+  convert_values_to_numeric: true # Set this to true if you want numeric values to be converted
+}
+
+SmarterCSV.process(database_file_path, options) do |chunk|
+  District.create(chunk) # Replace "YourModel" with the appropriate model name
 end
