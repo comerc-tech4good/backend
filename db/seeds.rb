0854ea5d270a67260ecfa531740f1c553a822a70
Nestor Martinez <nesturron@gmail.com>
2023-06-20 17:46:38 +0200

    created models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Activity.destroy_all
+
+column_names = File.readlines('db/data/message.txt').map(&:strip)
+
+p column_names
+
+column_names[7..column_names.size - 1].each do |column_name|
+  Activity.create!(
+    name: column_name.capitalize.gsub('_', ' ')
+  )
+end
